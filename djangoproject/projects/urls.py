59e1fdca06b30be0cccf3a69820ed4026f791330
bhogendra1412 <bhogendra488@gmail.com>
2025-04-22 23:47:13 +0530

    Updated my website

--- djangoproject/projects/urls.py
+++ djangoproject/projects/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ProjectListCreateView, ProjectDetailView, CustomAuthToken, index, RegisterView
 
 urlpatterns = [
-    path('home/', index, name='projects_index'),
+    path('', index, name='projects_index'),
     path('projects/', ProjectListCreateView.as_view(), name='project-list'),
     path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
     path('login/', CustomAuthToken.as_view(), name='login'),
